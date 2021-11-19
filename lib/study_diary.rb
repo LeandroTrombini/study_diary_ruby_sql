a8f4fe193c832de8cd920a58f85f8863eeb5b837
Leandro Trombini <leandro.ae@gmail.com>
2021-11-19 11:09:15 -0300

    'Organizando'

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -2,7 +2,6 @@ require_relative 'category'
 require_relative 'study_item'
 
 
-
 class StudyDiary
     attr_accessor :items
     def initialize(items: [])
@@ -12,15 +11,16 @@ class StudyDiary
 
     def menu
         opcao_selecionada = 0
-        while opcao_selecionada != 6
+        while opcao_selecionada != 7
             opcoes_menu = <<~MENU           
             ========= Menu ==========\n
         [1] Cadastrar um item para estudo
         [2] Remover um item 
         [3] Ver todos os itens cadastrados
         [4] Buscar um item         
-        [5] Concluído        
-        [6] Sair        
+        [5] Buscar itens concluídos         
+        [6] Marcar como concluído        
+        [7] Sair        
         Escolha uma opção:
         MENU
 
@@ -45,8 +45,10 @@ class StudyDiary
         when 4
             procurar_item
         when 5
-            concluido
+            procurar_item_concluido
         when 6
+            concluido
+        when 7
           puts "Você saiu do Diário de Estudos"
         else
           puts "Opção não encontrada. Tente novamente."
@@ -55,6 +57,7 @@ class StudyDiary
     end
 
     def cadastrar_item
+      items = StudyItem.all
         puts "Digite o título do item: "
         nome = gets().chomp().to_s       
 
@@ -66,16 +69,16 @@ class StudyDiary
           
         id_item = items.length + 1
         puts
-        items = StudyItem.new(id: id_item, title: nome, category: categoria, descr: descricao, done: done)
         StudyItem.save_to_db(id_item, nome, categoria, descricao, 'Em andamento')
-        puts "Item cadastrado!"     
         
+        puts "Item cadastrado!"          
         
     end
 
     def imprimir_itens(buscar_categoria = '')
       items = StudyItem.all
         puts "========= Títulos cadastrados =========="
+        puts
         if buscar_categoria == ''
             items.each do |item|
             puts "[#{item.id}] - Nome: #{item.title} / Categoria: #{item.category.name} / Descrição: #{item.descr} / Status: #{item.done}"      
@@ -95,45 +98,38 @@ class StudyDiary
     end
 
     def remover_item
-        puts "Digite o código do item a ser deletado:"
+      items = StudyItem.all
+        puts "Digite o código do item a ser deletado:"         
             item_para_deletar = gets().chomp().to_i
-            if item_para_deletar <= items.length
-              items.delete_at(item_para_deletar - 1)
+
+            if item_para_deletar != 0  
+              StudyItem.delete_by_id(item_para_deletar)              
               puts "#{item_para_deletar} removido!"
             else
               puts "Código não localizado. Tente novamente."
-            end
-
+            end            
       end
 
       def procurar_item
-        puts "Digite o nome do item que deseja buscar:"
-    
-        item = gets().chomp().to_s
-    
-        puts "Items encontrados"
-    
-        item_buscado = items.select {|i| i.title.downcase.include? item.downcase}
-        if item_buscado.length != 0
-            item_buscado.each do |item|
-            puts "[#{item.id}] - #{item.title} / Categoria: #{item.category.name} / Descrição: #{item.descr} / Status: #{item.done}"
-          end
-          puts "\n\n"
-        else
-          puts "Nenhum item encontrado com o termo de busca"
-        end
-    
+        items = StudyItem.all
+        puts "Digite o nome do item que deseja buscar:"       
+        item = gets().chomp().to_s    
+            
+            StudyItem.find_by_title(item)    
+      end
+
+      def procurar_item_concluido
+                      
+            StudyItem.all_done               
       end
 
 
       def concluido 
-        puts "Código a ser concluído: "
+        items = StudyItem.all
+        puts "Escolha um item para ser concluído: "
+        puts
         item_para_concluir = gets().chomp().to_i
-        if item_para_concluir <= items.length
-          items[item_para_concluir - 1].done = 'Concluído'
-        else
-          puts "Código não localizado. Tente novamente."
-        end
+        StudyItem.done(item_para_concluir)
 
       end
     
--- lib/study_item.rb
+++ lib/study_item.rb
@@ -24,12 +24,54 @@ class StudyItem
 
       def self.save_to_db(id, title, category, descr, done)
         db = SQLite3::Database.open 'db/database.db'
-        db.execute "INSERT INTO task VALUES('#{ id }', '#{ title }', '#{ category }', '#{ descr }', '#{ done }')"
+        db.execute "INSERT INTO tasks VALUES('#{ id }', '#{ title }', '#{ category }', '#{ descr }', '#{ done }')"
         db.close
     
         self
       end
 
-     
+      def self.delete_by_id(id)
+        db = SQLite3::Database.open "db/database.db"
+        db.results_as_hash = true
+        tasks = db.execute "DELETE FROM tasks WHERE id=#{id}"
+        db.close
+      end
 
+      def self.find_by_title(title)
+        db = SQLite3::Database.open "db/database.db"
+        db.results_as_hash = true
+        tasks = db.execute "SELECT id, title, category, descr, done FROM tasks where (title LIKE'%#{title}%' OR descr LIKE'%#{title}%')"
+        db.close
+        tasks.map {|task| new(id: task['id'], title: task['title'], category: task['category'], descr: task['descr'], done: task['done']) }
+        if tasks.length == 0
+          return print "Não foi encontrado nenhum item."
+        else
+          print "Resultado da busca:"
+          puts
+          tasks.each { |task| print "\n#{task['id']} - #{task['title']} - #{task['category']} - #{task['descr']} - #{task['done']}\n"}
+          puts
+        end
+      end
+
+      def self.all_done
+        db = SQLite3::Database.open 'db/database.db'
+        db.results_as_hash = true
+        tasks = db.execute  "SELECT id, title, category, descr, done FROM tasks where done LIKE 'Concluído'"
+        db.close    
+        tasks.map do |task|
+          new(id: task['id'], title: task['title'], category: task['category'], descr: task['descr'], done: task['done'])
+        print "Os itens concluídos são: "
+        puts
+        tasks.each { |task| print "\n#{task['id']} - #{task['title']} - #{task['category']} - #{task['descript']} - #{task['done']}\n"}
+        puts
+        end
+      end
+     
+      def self.done(id)
+        db = SQLite3::Database.open "db/database.db"
+        db.results_as_hash = true
+        tasks = db.execute "UPDATE tasks SET done = 'Concluído' WHERE id=#{id}"
+        db.close
+        
+      end
 end
